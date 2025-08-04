1e7e07f26b2e6d13ca3421f1c78e9cbe8bd516ce
Girgis <girgisemad74@gmail.com>
2025-08-04 05:47:26 +0300

    this revision

--- d1_JSrevision/this.js
+++ d1_JSrevision/this.js
@@ -11,9 +11,60 @@ Object.prototype.greet = function() {
 	console.log(`Hello, my name is ${this.name}`);
 }
 
-girgis.greet(); // Hello, my name is Girgis
-adham.greet(); // Hello, my name is Adham
+// girgis.greet(); // Hello, my name is Girgis
+// adham.greet(); // Hello, my name is Adham
 
 // so in this case the this is the object that calls the method
+
+const friends = {
+	name: "Friends",
+	firstFriend: {
+		greet: function() {
+			console.log(this);
+			console.log(`Hello, my name is ${this.name}`);
+		}
+	},
+	secondFriend: {
+		name: "Second Friend",
+		greet: function() {
+			console.log(this);
+			console.log(`Hello, my name is ${this.name}`);
+		}
+	}
+}
+
+// friends.firstFriend.greet();
+// Hello, my name is undefined
+// because the this is the firstFriend object, but it doesn't have a name property
+// friends.secondFriend.greet();
+// Hello, my name is Second Friend
+
+
+function outer() {
+	const arrowGreet = () => console.log(this.name);
+	arrowGreet();
+}
+
+// const obj = { name: 'Alice' };
+// outer.call(obj);
+//Output: Alice
+// outer();
+//Output: undefined (in strict mode) or window.name (non-strict mode)
+
+
+const obj2 = {
+	name: 'Bob',
+	regularMethod: function() {
+		const arrowGreet = () => console.log(`Hello, ${this.name}!`);
+		arrowGreet();
+	}
+};
+
+obj2.regularMethod();
+// Output: Hello, Bob!
+const unbound = obj2.regularMethod;
+unbound();
+// Output: Hello, undefined! (in strict mode)
+
 
 
